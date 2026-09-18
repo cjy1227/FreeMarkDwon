7a20634a09081a0abb31db9adb22f412cee6f3e3
account: cjy1227 <account: cjy1227@users.noreply.github.com>
2026-09-19 00:03:22 +0800

    Match Typora GitHub theme typography

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import '@fontsource/open-sans/400.css'
+import '@fontsource/open-sans/400-italic.css'
+import '@fontsource/open-sans/700.css'
+import '@fontsource/open-sans/700-italic.css'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
