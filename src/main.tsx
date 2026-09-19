444d8c5d0e8dffb25bc9d4ca912ddee577322f39
account: cjy1227 <account: cjy1227@users.noreply.github.com>
2026-09-19 11:46:10 +0800

    Improve code block contrast

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,6 @@ import '@fontsource/open-sans/400-italic.css'
 import '@fontsource/open-sans/700.css'
 import '@fontsource/open-sans/700-italic.css'
 import './styles.css'
+import './code-contrast.css'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
